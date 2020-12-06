03d62ebec9c209f472a7ef970c7be5f1695ecc86
Chalisa <ploy_603_f@hotmail.com>
2020-12-06 14:38:46 +0700

    create code index sign page and js and firebase

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyATWQVYNfuJ4DMymwiZlW0jSvq0pr1TLe4",
+  authDomain: "clinic-on-26919.firebaseapp.com",
+  databaseURL: "https://clinic-on-26919.firebaseio.com",
+  projectId: "clinic-on-26919",
+  storageBucket: "clinic-on-26919.appspot.com",
+  messagingSenderId: "323160925182",
+  appId: "1:323160925182:web:ce56f995f084cbc9bc96af",
+  measurementId: "G-DC9XJ2BB02"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ index.js
@@ -0,0 +1,30 @@
+$(function () {
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      //User is signed in.
+      displayName = user.displayName;
+      email = user.email;
+      photoUrl = user.photoURL;
+      console.log(displayName,email,photoUrl);
+
+      $("#username").text(email);
+      $("#displayName").text(displayName);
+      $("#photo").attr("src",photoURL);
+
+    } else {
+      window.location.href = 'signin.html'
+    }
+  });
+
+  $("#signout").click(function () {
+    firebase.auth().signOut()
+      .then(function () {
+        // Sign-out successful.
+      }).catch(function (error) {
+        // An error happened.
+      });
+
+  });
+
+})
