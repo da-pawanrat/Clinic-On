163b88d4ff59fd1c03c525928c99fd5a91593069
Chalisa <ploy_603_f@hotmail.com>
2020-12-06 15:18:09 +0700

    add profile

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-$(function () {
+// $(function () {
 
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      //User is signed in.
-      displayName = user.displayName;
-      email = user.email;
-      photoUrl = user.photoURL;
-      console.log(displayName,email,photoUrl);
+//   firebase.auth().onAuthStateChanged(function (user) {
+//     if (user) {
+//       //User is signed in.
+//       displayName = user.displayName;
+//       email = user.email;
+//       photoUrl = user.photoURL;
+//       console.log(displayName,email,photoUrl);
 
-      $("#username").text(email);
-      $("#displayname").text(displayName);
-      $("#photo").attr("src",photoUrl);
+//       $("#username").text(email);
+//       $("#displayname").text(displayName);
+//       $("#photo").attr("src",photoUrl);
 
-    } else {
-      window.location.href = 'signin.html'
-    }
-  });
+//     } else {
+//       window.location.href = 'signin.html'
+//     }
+//   });
 
-  $("#signout").click(function () {
-    firebase.auth().signOut()
-      .then(function () {
-        // Sign-out successful.
-      }).catch(function (error) {
-        // An error happened.
-      });
+//   $("#signout").click(function () {
+//     firebase.auth().signOut()
+//       .then(function () {
+//         // Sign-out successful.
+//       }).catch(function (error) {
+//         // An error happened.
+//       });
 
-  });
+//   });
 
-})
+// })
